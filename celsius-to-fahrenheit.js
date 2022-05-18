ec05794578197acfa668bd56220611d4a5cd482f
IanMcbull <ianmugenya@gmail.com>
2022-05-18 13:40:55 +0300

    Added more comments to the solution to challenge one

--- celsius-to-fahrenheit.js
+++ celsius-to-fahrenheit.js
@@ -3,7 +3,10 @@ You are given a variable celsius representing a temperature in Celsius. Use the 
 */
 
 // The formula to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
-
+// We need to put brackets around 9/5 because we want that expression to be calculated first.
+//Brackets will take precedence over any other operation
+// This is how the operation will be carried out
+// 9/5 * celsius +32
 module.exports = (celsius)=>{
  return celsius * (9/5) + 32
 }
