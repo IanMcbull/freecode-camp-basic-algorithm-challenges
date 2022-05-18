a7c0384f4719102e3cb8df5d35c820cb7906ba98
IanMcbull <ianmugenya@gmail.com>
2022-05-18 08:21:39 +0300

    Made changes to solution 3

--- factorilize.js
+++ factorilize.js
@@ -5,15 +5,15 @@ Factorials are often represented with the shorthand notation n!
 
 */
 const factorilize = (num) =>{
- if(num === 1){
-     return num
+ if(num <= 1){
+     return 1
  }
  return num * factorilize(num-1)
 }
-factorilize(5)
+console.log(factorilize(0))
 
 /*
 For this challenge I decided to use recursion
-As long as the number is equal to 1 the function will return 1
+As long as the number is equal to or less than 1 the function will return 1
 If the number is greater than one, the function will call itself
 */
