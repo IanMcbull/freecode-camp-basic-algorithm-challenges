f73b613afdd2f122b11300e13699696a4b41dbe8
IanMcbull <ianmugenya@gmail.com>
2022-05-19 11:04:04 +0300

    This is my solution to challenge fourteen

---
+++ whereDoIBelong.js
@@ -0,0 +1,27 @@
+/*
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+*/
+
+const getIndexToIns = (arr,num) =>{
+ if(arr.length === 0){
+  return 0;
+ } 
+   arr= arr.sort((a,b)=>a-b);
+    for (const element of arr) {
+     if(element >= num){
+        return arr.indexOf(element);
+     }
+     if(num > arr[arr.length - 1]){
+         return arr.length
+     }
+ }
+}
+
+getIndexToIns([10, 20, 30, 40, 50], 35);
+
+/*
+The first thing I did, was to check whether the array had any values. If it didn't, then return zero.
+if the array is not empty, then loop through the array and check whether the current element is greater or equal to the number provided. If this condition is met, then return the index of that element.
+If the number provided is bigger than the last element in the array after it's been sorted,then we return the length of the array. 
+ */
